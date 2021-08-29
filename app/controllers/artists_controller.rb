cd1a57c09c7d1c04159855688d38cd183afb31fb
Jamie Pace <jamiejpace@gmail.com>
2021-08-28 19:46:16 -0600

    Add update and edit

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,10 +8,21 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    @artist = Artist.create(artist_params)
+    artist = Artist.create(artist_params)
     redirect_to "/artists"
   end
 
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    artist = Artist.find(params[:id])
+    artist.update(artist_params)
+    redirect_to '/artists'
+  end
+
+private
   def artist_params
     params.permit(:name)
   end
